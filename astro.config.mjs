ee1ca0732f0e8df80932e6c33bebedebcac0a2ec
mattiasalvato <mattiasalvatosg@gmail.com>
2026-06-10 01:02:20 +0200

    Configurazione deploy su GitHub Pages con Actions

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // 1. Sostituisci con il tuo nome utente di GitHub e il nome della repository
+  site: 'https://mattiasalvato.github.io',
+  base: '/sambo-bike-rent-repo',
+});
